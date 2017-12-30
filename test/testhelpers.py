8f2d86575e20814ca9d37884a0e915aeb3b8344a
ogoyal <ojasgoyal@gmail.com>
2017-12-30 00:57:02 -0800

    adding travis

--- test/testhelpers.py
+++ test/testhelpers.py
@@ -3,9 +3,14 @@ import unittest
 
 class TestMonty(unittest.TestCase):
     def test_trial(self):
-        num_trial = 150
+        num_trial = 1000
         settings.set_trial_number(num_trial) 
-        self.assertEqual(settings.get_trial_number(), 150)
+        self.assertEqual(settings.get_trial_number(), 1000)
+
+    def test_sections(self):
+        num_sections = 100
+        settings.set_sections(num_sections)
+        self.assertEqual(settings.get_sections(), 100)
 
 if __name__ == '__main__':
     unittest.main()
