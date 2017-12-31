35956c5266e7213064faeb64e71a074407fe218f
U-DESKTOP-LOAN0SQ\rk <ojasgoyal@gmail.com>
2017-12-30 23:09:16 -0800

    fix error with import

--- test/testcontrollers.py
+++ test/testcontrollers.py
@@ -3,7 +3,7 @@ import unittest
 
 class TestController(unittest.TestCase):
     def test_choose(self):
-        random_num = controllers.choose()
+        random_num = monty.choose()
         self.assertTrue(0 <= random_num <= 2)
 
 if __name__ == '__main__':
