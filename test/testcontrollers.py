00378f999a7db7b51b5501574edc82b202aaa7e3
U-DESKTOP-LOAN0SQ\rk <ojasgoyal@gmail.com>
2017-12-30 21:12:31 -0800

    test controllers

---
+++ test/testcontrollers.py
@@ -0,0 +1,10 @@
+from controllers import monty
+import unittest
+
+class TestController(unittest.TestCase):
+    def test_choose(self):
+        random_num = controllers.choose()
+        self.assertTrue(0 <= random_num <= 2)
+
+if __name__ == '__main__':
+    unittest.main()
